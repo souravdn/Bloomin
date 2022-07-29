a158184a676cf066da788809a6b8e6fd65945e26
Sailab Banik <sailabbanik24@gmail.com>
2022-07-30 02:49:11 +0530

    Disabled Night/Dark Mode.

--- app/src/main/java/com/example/simplecalc/MainActivity.java
+++ app/src/main/java/com/example/simplecalc/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.simplecalc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 }
