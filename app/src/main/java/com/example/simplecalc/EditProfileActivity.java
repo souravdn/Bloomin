e6259a75534669c5ef2b3d22226faf8a2005919f
Sailab Banik <sailabbanik24@gmail.com>
2022-07-31 21:44:45 +0530

    User Permissions for Storage and Location and Fetch Location from GPS.

--- app/src/main/java/com/example/simplecalc/EditProfileActivity.java
+++ app/src/main/java/com/example/simplecalc/EditProfileActivity.java
@@ -1,13 +1,25 @@
 package com.example.simplecalc;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -17,20 +29,25 @@ import android.widget.AutoCompleteTextView;
 import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
-public class EditProfileActivity extends AppCompatActivity {
+public class EditProfileActivity extends AppCompatActivity implements LocationListener {
 
+    int storage_permission_code = 1;
     ImageView profile;
     ImageButton pencil;
     String[] gender = {"Male", "Female", "Other"};
     AutoCompleteTextView autoCompleteTextView;
-    TextInputEditText bday;
+    TextInputEditText bday, user_location;
     DatePickerDialog.OnDateSetListener onDateSetListener;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +58,11 @@ public class EditProfileActivity extends AppCompatActivity {
         pencil = findViewById(R.id.pencil_btn);
         autoCompleteTextView = findViewById(R.id.edit_gender);
         bday = findViewById(R.id.edit_bday);
+        bday.setShowSoftInputOnFocus(false);
+        bday.setCursorVisible(false);
+        user_location = findViewById(R.id.edit_location);
+        user_location.setShowSoftInputOnFocus(false);
+        user_location.setCursorVisible(false);
 
         final Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
@@ -54,12 +76,34 @@ public class EditProfileActivity extends AppCompatActivity {
         pencil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ImagePicker.Companion.with(EditProfileActivity.this)
-                        .crop()
-                        .cropSquare()
-                        .compress(1024)
-                        .maxResultSize(1080, 1080)
-                        .start();
+                if(ContextCompat.checkSelfPermission(EditProfileActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    ImagePicker.Companion.with(EditProfileActivity.this)
+                            .crop()
+                            .cropSquare()
+                            .compress(1024)
+                            .maxResultSize(1080, 1080)
+                            .start();
+                }
+                else {
+                    requestStoragePermission();
+                }
+            }
+        });
+
+        user_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(EditProfileActivity.this, "Wait for Few Seconds", Toast.LENGTH_SHORT).show();
+                if(ContextCompat.checkSelfPermission(EditProfileActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(EditProfileActivity.this, new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    }, 100);
+                }
+                else{
+                    getLocation();
+                }
             }
         });
 
@@ -73,6 +117,8 @@ public class EditProfileActivity extends AppCompatActivity {
                 datePickerDialog.show();
             }
         });
+
+
         onDateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
@@ -81,6 +127,7 @@ public class EditProfileActivity extends AppCompatActivity {
                 bday.setText(date);
             }
         };
+
     }
 
     @Override
@@ -91,5 +138,72 @@ public class EditProfileActivity extends AppCompatActivity {
         profile.setImageURI(uri);
     }
 
+    private void requestStoragePermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission Needed")
+                    .setMessage("Permission is Needed to set Profile Picture.")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(EditProfileActivity.this,
+                                    new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    storage_permission_code);
 
+                        }
+                    })
+                    .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .create().show();
+        }
+        else{
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, storage_permission_code);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == storage_permission_code) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                ImagePicker.Companion.with(EditProfileActivity.this)
+                        .crop()
+                        .cropSquare()
+                        .compress(1024)
+                        .maxResultSize(1080, 1080)
+                        .start();
+            }
+            else{
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+        try {
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1, EditProfileActivity.this);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        try{
+            Geocoder geocoder = new Geocoder(EditProfileActivity.this, Locale.getDefault());
+            List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            String address = addressList.get(0).getAddressLine(0);
+            user_location.setText(address);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
