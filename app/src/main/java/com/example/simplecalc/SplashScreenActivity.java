9b30f0fe10bfc0e2aca8894a364440a388f8af0f
Sourav Debnath <souravdebnath.code@gmail.com>
2022-08-04 03:26:23 +0530

    Sign in,Sign Out function on app opening

--- app/src/main/java/com/example/simplecalc/SplashScreenActivity.java
+++ app/src/main/java/com/example/simplecalc/SplashScreenActivity.java
@@ -16,7 +16,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                startActivity(new Intent(SplashScreenActivity.this, signup.class));
                 finish();
             }
         },1500);
